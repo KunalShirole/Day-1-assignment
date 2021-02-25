326b432bc47f68f0300b7f7a38c6f07256a0aac2
KunalShirole <79651766+KunalShirole@users.noreply.github.com>
2021-02-25 21:43:01 +0530

    Add files via upload

---
+++ Assignmentday1.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+x=int(input("Enter the first number: "))
+y=int(input("Enter the second number: "))
+
+result=0
+result=x^y
+
+print(x,^,y,": ",result)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ assignmentday1,2.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+x=int(input("Enter the first number: "))
+y=int(input("Enter the second number: "))
+
+
+
+result=x**y
+
+print(x,"^",y,": ",result)
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
